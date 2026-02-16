0f0969bb5a7d9742efc0e2a1f8ed79f007f20ba0
Mohammed Sadiq <contact.mohammedsadiq@gmail.com>
2026-02-16 12:54:42 +0530

    Update Router basename for custom domain

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import ModulePage from './pages/ModulePage';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename="/Factosiem">
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/module/:moduleId" element={<ModulePage />} />
-      </Routes>
+    <Router basename="/">
+      {/* Your routes go here */}
     </Router>
   );
 }
